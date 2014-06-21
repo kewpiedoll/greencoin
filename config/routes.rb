26d3c96657e847c466141ef0db89306e3619bf1f
Chris Ashfield <biodieselchris@gmail.com>
2014-06-21 15:46:43 -0700

    out page

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Greencoin::Application.routes.draw do
   match '/about',     to: 'static_pages#about',   via: 'get'
   match '/contact',   to: 'static_pages#contact', via: 'get'
   match '/news',      to: 'static_pages#news', via: 'get'
-
+  match '/out',      to: 'static_pages#out', via: 'get'
   
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
